367566ab242f564f56ac57993a4dddc2925d34b0
Javier Jacobo <javier.jacobo@hotmail.com>
2020-08-21 15:47:41 -0700

    github pages

---
+++ build/precache-manifest.8753cc0145330771db1ed57d6b621449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4aaca63e140b80c9a3c9aa6f38b6a3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a0ce5d77031609f8d69",
+    "url": "/static/css/main.be350e58.chunk.css"
+  },
+  {
+    "revision": "8345300188afe5489b45",
+    "url": "/static/js/2.090c33ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.090c33ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0ce5d77031609f8d69",
+    "url": "/static/js/main.1beed02c.chunk.js"
+  },
+  {
+    "revision": "72c07dcb21740fe3cff4",
+    "url": "/static/js/runtime-main.b593c7cf.js"
+  }
+]);
