7e370f5ca9262e1217739b5428d1773d57a5cbc0
Javier Jacobo <javier.jacobo@hotmail.com>
2020-08-21 16:08:23 -0700

    google pages

---
+++ docs/precache-manifest.e760c1091c1c7319d92a93d12a638e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26526c9046ba624940038cf61d9ac65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d751b90eda39bdd0a354",
+    "url": "/static/css/main.52528c13.chunk.css"
+  },
+  {
+    "revision": "8345300188afe5489b45",
+    "url": "/static/js/2.090c33ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.090c33ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d751b90eda39bdd0a354",
+    "url": "/static/js/main.1beed02c.chunk.js"
+  },
+  {
+    "revision": "72c07dcb21740fe3cff4",
+    "url": "/static/js/runtime-main.b593c7cf.js"
+  }
+]);
